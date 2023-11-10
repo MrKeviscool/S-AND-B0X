e37c4310cd152b320daf10dbb126041f504aad7b
MrKeviscool <77475385+MrKeviscool@users.noreply.github.com>
2023-11-10 20:43:04 +1030

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-g++ -c sandboxgraphics.cpp
-g++ sandboxgraphics.o -o sbox -lsfml-graphics -lsfml-window -lsfml-system
+g++ -c sandbox.cpp
+g++ sandbox.o -o sbox -lsfml-graphics -lsfml-window -lsfml-system
 ./sbox
